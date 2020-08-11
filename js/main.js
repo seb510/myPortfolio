097e888a01d4ef1a4af4c2a4af3760cc531f38af
Nikolay <sebistian@ukr.net>
2020-08-11 19:41:37 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Скрипт для плавного перехода для якорных ссылок по сайту
+$(document).ready(function(){
+    $("#navigation").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
