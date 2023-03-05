efe0acbc8272004e6eaae18071535826bc686f11
Nikolay <34944975+seb510@users.noreply.github.com>
2023-03-05 19:43:14 +0200

    add  scorll
    fixed scorll to section/ added fixed menu and header

--- js/main.js
+++ js/main.js
@@ -1,15 +1,28 @@
-$(document).ready(function(){
-// Скрипт для плавного перехода для якорных ссылок по сайту
-var $page = $('html, body');
-$('a[href*="#"]').click(function() {
-    $page.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 800);
-    return false;
-});
+jQuery(document).ready(function($) {
 
-//Burger menu
-    $('.menu-btn').click(function(){
-        $('.menu__list').slideToggle('500');
+    const scrollHeader = () => {
+        $(window).scroll(function() {
+            let sticky = $('.header'),
+                scroll = $(window).scrollTop();
+
+            if (scroll >= 100) sticky.addClass('scroll');
+            else sticky.removeClass('scroll');
+        });
+    }
+
+    scrollHeader();
+
+    // Скрипт для плавного перехода для якорных ссылок по сайту
+    $(document).on('click', 'a[href^="#"]', function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
     });
+
+    //Burger menu
+    $(document).on('click', '.menu-btn', () => {
+        $('.menu__list').stop().slideToggle('500');
+        $('body').toggleClass('hidden');
+    })
 });
